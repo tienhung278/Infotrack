a6c4c774a159619c256e36107f723035536c9773
tienhung278 <tienhung278@gmail.com>
2024-06-29 20:09:06 +0800

    Implement search form

--- Infotrack.APP/src/app/app.component.ts
+++ Infotrack.APP/src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Infotrack.APP';
+export class AppComponent implements OnInit {
+  searchEngines:[];
+  results;
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.searchEngines = [];
+    this.results = '';
+    this.myForm = fb.group({
+      numOfResults: 100,
+      keyword: 'land registry search',
+      websiteUrl: 'www.infotrack.co.uk',
+      searchEngineId: ''
+    });
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onSubmit(myForm: FormGroup) {
+
+  }
 }
